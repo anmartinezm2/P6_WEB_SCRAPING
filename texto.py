13a1a99bb5814c8f2a83ee437d37f333d74bf9bf
anmartinezm2 <111255059+anmartinezm2@users.noreply.github.com>
2026-09-10 10:23:00 -0600

    Update texto.py

--- texto.py
+++ texto.py
@@ -41,5 +41,5 @@ df.to_csv("catalogo_bissu.csv", index=False)
 print("Scraping exitoso y archivo catalogo_bissu.csv creado.")
 """
 
-with open("scraper.py", "w", encoding="utf-8") as f:
+with open("texto.py", "w", encoding="utf-8") as f:
     f.write(script_code)
